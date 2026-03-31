d0b05e514be324359c2db3ed19ab513cfe4c34a0
Victor Ruiz <veruiz@uc.cl>
2026-03-30 22:44:01 -0300

    feat(packages): mejora la tipificación en PackagesController y corrige la importación de PrismaService

--- src/packages/packages.controller.ts
+++ src/packages/packages.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
-import { PackagesService } from './packages.service';
+import {
+  PackagesService,
+  CreatePackageDto,
+  GetPackagesQuery,
+} from '@packages/packages.service';
 
 @Controller('packages')
 export class PackagesController {
   constructor(private readonly packagesService: PackagesService) {}
 
   @Post()
-  createPackage(@Body() data: any) {
+  createPackage(@Body() data: CreatePackageDto) {
     return this.packagesService.createPackage(data);
   }
 
   @Get()
-  getPackages(@Query() query: any) {
+  getPackages(@Query() query: GetPackagesQuery) {
     return this.packagesService.getPackages(query);
   }
 
