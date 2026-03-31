3c59ae92b8cc8e4352b21fe3974a51c12536faa9
Victor Ruiz <veruiz@uc.cl>
2026-03-30 22:43:34 -0300

    feat(app): actualiza la importación de PackagesModule para usar el alias de paquete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PackagesModule } from './packages/packages.module';
+import { PackagesModule } from '@packages/packages.module';
 
 @Module({
   imports: [PackagesModule],
