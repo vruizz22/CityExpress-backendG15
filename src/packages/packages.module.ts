f733e753d46260b4b7631a464a42af73f46a5af3
Victor Ruiz <veruiz@uc.cl>
2026-05-04 00:06:09 -0400

    feat(packages): add RoutingModule to PackagesModule imports

--- src/packages/packages.module.ts
+++ src/packages/packages.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PackagesController } from '@packages/packages.controller';
 import { PackagesService } from '@packages/packages.service';
 import { PrismaService } from '@/prisma.service';
+import { RoutingModule } from '@/routing/routing.module';
 
 @Module({
+  imports: [RoutingModule],
   controllers: [PackagesController],
   providers: [PackagesService, PrismaService],
 })
